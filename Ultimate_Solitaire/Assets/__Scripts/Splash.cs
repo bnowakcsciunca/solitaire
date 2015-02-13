3ea4dc435ac755462b0a3bfef816d0ea0b73f4fe
dasBookOfGit1707 <dasbookofchrome1012@gmail.com>
2015-02-13 11:09:16 -0500

    Screen Logic Implemented
    (1) There are now four screens:
    Splash for the splash image
    Start for the start menu
    Game for the game
    ExitScreen for win or loss screen
    
    The splash screen stays up for an arbitrary amount of time that we can
    set by clicking the script component of the camera.
    Then the start screen loads, and we can click new game to start a game,
    help for info (not yet implemented), and exit to....exit.
    
    (2) I've also attached an Ultimate_Solitaire c# to the main camera in
    the game scene, so we can use that for the game logic if we need to.

---
+++ Ultimate_Solitaire/Assets/__Scripts/Splash.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Splash : MonoBehaviour {
+
+	public float		splashTime = 2.0f; // How long we want the splash screen to last
+	public bool			_________________;
+
+	void Update () {
+		splashTime -= Time.deltaTime;
+		if (splashTime <= 0) {
+			Application.LoadLevel ("2_Start");
+		}
+	}
+}
---
+++ Ultimate_Solitaire/Assets/__Scripts/Start_Screen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Start_Screen : MonoBehaviour {
+
+	public void newClicked() {
+		Application.LoadLevel ("3_Game");
+	}
+
+	public void helpClicked() {
+		// Switch the UI to the help UI
+	}
+
+	public void exitClicked() {
+		Application.Quit();
+	}
+}
