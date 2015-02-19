ee070bffa9020165aa9fe043c88bbc8a0ccf488e
Brendan Mulhern <bmmacomb@sbcglobal.net>
2015-02-19 15:43:11 -0500

    Minor fixes

--- Ultimate_Solitaire/Assets/__Scripts/Ultimate_Solitaire.cs
+++ Ultimate_Solitaire/Assets/__Scripts/Ultimate_Solitaire.cs
@@ -2,14 +2,25 @@
 using System.Collections;
 
 public class Ultimate_Solitaire : MonoBehaviour {
-
+	static public Ultimate_Solitaire S;
+	public Deck deck;
+	public TextAsset deckXML;
+	
+	void Awake(){
+		
+		S = this;
+	}
+	
 	// Use this for initialization
 	void Start () {
-	
+		deck = GetComponent<Deck> ();
+		deck.InitDeck (deckXML.text);
+		Deck.Shuffle (ref deck.cards); // shuffles deck
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
