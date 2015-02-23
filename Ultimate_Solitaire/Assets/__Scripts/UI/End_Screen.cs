2b200417d301651b6fc49ba0c4eea0b564552e9f
dasBookOfGit1707 <dasbookofchrome1012@gmail.com>
2015-02-23 12:13:32 -0500

    Created end screen, tidied up help text
    The end screen is now fully functional. You just have to load it and
    call the win or lose screen depending on whether you've won or lost
    (once the game is working we can add the function call.)
    In addition, the three buttons will either reload the game, go to main
    menu, or quit.
    
    I also formatted the help text and also made it more succinct.

--- Ultimate_Solitaire/Assets/__Scripts/UI/End_Screen.cs
+++ Ultimate_Solitaire/Assets/__Scripts/UI/End_Screen.cs
@@ -1,25 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class End_Screen : MonoBehaviour {
 
+	public Text title;
+	public Text Score;
+	public Text HScore;
+	public Text congrats;
+	public bool _____________________________;
+
 	int score;
+	int highscore;
+	GameObject winScreen;
+	GameObject loseScreen;
 
 	// Use this for initialization
 	void Awake () {
-		
+		highscore = PlayerPrefs.GetInt ("UltimateHighScore");
+
+		// Testing here
+		//WonGame ();
+		LostGame();
+	}
+
+	public void WonGame() {
+		title.text = "Congratulations, You Won!";
+		Score.text = score.ToString ();
+		HScore.text = highscore.ToString ();
+
+		if (score > highscore) {
+			congrats.text = "Congratulations, you beat the high score!";
+			highscore = score;
+			PlayerPrefs.SetInt ("UltimateHighScore", highscore);
+		}
+
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	public void LostGame() {
+		title.text = "You Lost. Try again!";
+		Score.text = score.ToString ();
+		HScore.text = highscore.ToString ();
+
+		if (score > highscore) {
+			congrats.text = "Congratulations, you beat the high score!";
+			highscore = score;
+			PlayerPrefs.SetInt ("UltimateHighScore", highscore);
+		}
 	}
 
-	public void WonGame(int score) {
-
+	public void PlayAgain() {
+		Application.LoadLevel ("3_Game");
 	}
 
-	public void LostGame(int score) {
+	public void MainMenu() {
+		Application.LoadLevel ("2_Start");
+	}
 
+	public void EndGame() {
+		Application.Quit ();
 	}
 }
